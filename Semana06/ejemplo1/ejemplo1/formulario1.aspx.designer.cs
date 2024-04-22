dfd825f91cb8874f8ed2c53360fe0a2363d75437
ArielTapia <arieltapia42@gmail.com>
2024-04-22 16:07:15 -0300

    Actualizar

--- Semana06/ejemplo1/ejemplo1/formulario1.aspx.designer.cs
+++ Semana06/ejemplo1/ejemplo1/formulario1.aspx.designer.cs
@@ -58,5 +58,14 @@ namespace ejemplo1
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DropDownList ddlNombre;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
